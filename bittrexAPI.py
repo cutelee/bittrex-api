7b405e6fb8ee5b745b83b8246b75cad8727db871
cutelee <cutelee2@khu.ac.kr>
2018-05-17 18:49:35 +0900

    Version 2.0

--- bittrexAPI.py
+++ bittrexAPI.py
@@ -6,31 +6,28 @@ import pymysql
 import datetime
 from operator import itemgetter
 from bittrex import Bittrex
+import pymysql
 
+# DB관련 변수
+ip = 'localhost' #서버호스트주소
+id = 'root' #DB이름
+pw = 'autoset' #DB비번
+name = 'bittrex'
+chs ='utf8'
+LOG_ID = "12.15테스트"
+#---DB연결
+conn = pymysql.connect(ip,id,pw,name,charset="utf8")
+curs = conn.cursor()
+sql_example = """insert into lsy_parsinglog(id,log)
+                   values (%s, %s)"""
+sql_bitcoin = """insert into lsy_bitcoin(stock,buy,sell,time) values (%s,%s,%s,%s)"""
 
 # used api version -> version 2
-API_V2_0 = 'v2.0'
+# API_V2_0 = 'v2.0'
 bit_API = Bittrex(None, None)
 
 # reference for trading volume(buy + sell)
-btc_refer = 100
-
-
-# return all markets name
-def get_all_markets():
-    res = bit_API.get_markets()
-    success = res["success"]
-    market_list = []
-    if success:
-        results = res["result"]         # type: list
-        for result in results:
-            market_list.append(result["MarketName"])
-
-        return market_list
-
-    else:
-        print("Failed!")
-        return None
+btc_refer = 1000
 
 
 # return marketName's history result
@@ -41,9 +38,8 @@ def get_market_history(MarketName):
     if success:
         return res["result"]
     else:
-        print("Failed!")
+        print("No History. Please Wait..")
         return None
-
 
 # make string to datetime (+9 hours)
 def make_datetime(timestamp):
@@ -53,123 +49,104 @@ def make_datetime(timestamp):
     #date_time = date_time + datetime.timedelta(hours=9)
     return date_time
 
-
-# select target market(under 100BTC volume) and return market_list
+# check target market - a stock that base volume under 100 BTC in 24 hours
 def check_target_market():
-    markets = get_all_markets()
-    market_list = []
-    # check all markets and add to market_list if market is targets
-    for market in markets:
-        his = bit_API.get_market_history(market)
-        result = get_market_history(market)
-        if not result == None:
-            buy = 0.0
-            sell = 0.0
-            standard_time = make_datetime('1970-01-01T12:00:00')
+    target_market_list = []
+    for tmp in bit_API.get_market_summaries()['result']:
+        if  tmp['BaseVolume'] < btc_refer:
+            if tmp['MarketName'].find('BTC-') != -1:
+                # print(tmp['MarketName'], tmp['BaseVolume'])       # for checking
+                target_market_list.append(tmp['MarketName'])
 
-            results = his["result"]
-            # check current market's all history
-            for result in results:
-                history_time = make_datetime(result["TimeStamp"])
-                # if current history_time is larger than standard_time: renew standard_time and buy, sell sum
-                if standard_time < history_time:
-                    buy = 0.0
-                    sell = 0.0
-                    standard_time = history_time
+    return target_market_list
 
-                # if current history_time is smaller than standard_time: ignore
-                if standard_time == history_time:
-                    type = result["OrderType"]
-                    # add time condition
-                    if type == 'BUY':
-                        buy += result["Total"]
-                    else:
-                        sell += result["Total"]
-
-            # if total volume under 100BTC, add market_list
-            if (buy+sell) < btc_refer:
-                market_list.append(market)
-                print(market)
-                print(standard_time)
-                # exception handling (divided by zero)
-                if buy == 0 and sell == 0:
-                    print("0 %% (%.2f / %.2f)" % (buy, buy + sell))
-                else:
-                    print("%.2f %% (%.2f / %.2f)" % ((buy / (buy + sell)) * 100, buy, buy + sell))
-
-                print("Buy: ", buy)
-                print("Sell: ", sell)
-
-    return market_list
-
-
-def make_last_time(market_list):
-    standard_time = make_datetime('1970-01-01T12:00:00')
-
-
-# get market list from 'stocks.txt' file
-def get_stocks_from_file():
-    fstream = open('stocks.txt', 'r')
-    stocks_list = fstream.read()
-    stocks = stocks_list.split(',')
-    stocks.pop()        # remove last blank list
-    fstream.close()
-    return stocks
-
-
-# write market list from 'stocks.txt' file
-# get market list by using check_target_market() function
-def write_stocks_to_file():
-    # check target market and rewrite 'stocks.txt' file
-    market_list = check_target_market()
-    fstream = open('stocks.txt', 'w')
-    for i in market_list:
-        fstream.write(i)
-        fstream.write(',')
-    fstream.close()
-
-
-# get param market's new 1'min interval trade data
-def get_trade_data(market_name, time_dic):
+# check target_market's 1min tics
+# at first: last_time = program start time
+def check_buy_n_sell(market_name, last_time):
+    # last_time: datetime, market_name: string
     histories = get_market_history(market_name)
+    default_time = make_datetime('1970-01-01T12:00:00')
+    before_check_time = default_time
     if not histories == None:
-        last_time = time_dic['market_name']
-        checking_time = last_time
+        # sorting histories by time
+        histories = sorted(histories, key=itemgetter('TimeStamp'))
+        print(market_name)          # for test
         buy = 0.0
         sell = 0.0
-        sorted_histories = sorted(histories, key=itemgetter('TimeStamp'))
-        for history in sorted_histories:
-            # check 'is current history time is greater than last check time'
+        print("Check")
+        for history in histories:
             current_time = make_datetime(history["TimeStamp"])
-            if current_time > last_time:
-                if current_time > checking_time:
-                    if buy != 0 or sell != 0:
-                        # insert db time, buy, sell, marketname
-                        buy = 0.0
-                        sell = 0.0
+            try:
+                if current_time > last_time:
+                    if before_check_time == current_time:
+                        # sum
+                        if history["OrderType"] == "BUY":
+                            buy += history["Total"]
+                        else:
+                            sell += history["Total"]
 
-                    last_time = checking_time
-                    checking_time = current_time
-                    if history["OrderType"] == "BUY":
-                        buy += history["Total"]
-                    else:
-                        sell += history["Total"]
+                        # if last, print sum and return last_time
+                        if history == histories[-1]:
+                            curs.execute(sql_bitcoin, (market_name,buy,sell,before_check_time))
+                            conn.commit()
+                            print(before_check_time)
+                            if buy == 0 and sell == 0:
+                                print("0 %% (%.2f / %.2f)" % (buy, buy + sell))
+                            else:
+                                print("%.2f %% (%.2f / %.2f)" % ((buy / (buy + sell)) * 100, buy, buy + sell))
+                            print("BUY: ", buy)
+                            print("SELL: ", sell)
 
-                elif current_time == checking_time:
-                    if history["OrderType"] == "BUY":
-                        buy += history["Total"]
-                    else:
-                        sell += history["Total"]
+                            last_time = before_check_time
+                            return last_time
 
+                    elif before_check_time < current_time:
+                        # print and renew sum data  -> for test (insert to DB)
+                        if before_check_time != default_time:
+                            curs.execute(sql_bitcoin, (market_name, buy, sell, before_check_time))
+                            conn.commit()
+                            print(before_check_time)
+                            if buy == 0 and sell == 0:
+                                print("0 %% (%.2f / %.2f)" % (buy, buy + sell))
+                            else:
+                                print("%.2f %% (%.2f / %.2f)" % ((buy / (buy + sell)) * 100, buy, buy + sell))
+                            print("BUY: ", buy)
+                            print("SELL: ", sell)
+                            last_time = before_check_time
 
-#target_market_list = check_target_market()
-#print(target_market_list)
-#print(target_market_list.__len__())
-target_market_list = get_stocks_from_file()
-for market in target_market_list:
-    get_trade_data(market)
+                            # clear buy and sell sum
+                            buy = 0.0
+                            sell = 0.0
 
+                        if history["OrderType"] == "BUY":
+                            buy += history["Total"]
+                        else:
+                           sell += history["Total"]
 
+                    before_check_time = current_time
 
+            except:
+                print("No Time. Please Wait..")
+                pass
 
+        return last_time
 
+SEC_TO_MINUTE = 60
+if __name__ =="__main__":
+    # get current time and insert to DB
+    now = datetime.datetime.now()
+    curs.execute(sql_example, (LOG_ID,now))
+    conn.commit()
+
+    target_markets = check_target_market()
+    time_dic = {}
+    # set time_dic 'now'
+    for market in target_markets:
+        time_dic[market] = now
+
+    while True:
+        for market in target_markets:
+            new_time = check_buy_n_sell(market, time_dic[market])
+            time_dic[market] = new_time
+            #print(time_dic[market])
+        time.sleep(0.5)
